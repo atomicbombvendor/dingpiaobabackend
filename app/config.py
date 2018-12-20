32f556b95eedd39269f0f7d473c7ce0f975a6d37
Unknown <atomicbombvendor@gmail.com>
2018-12-20 22:26:02 +0800

    使用独享型数据库

--- app/config.py
+++ app/config.py
@@ -1,11 +1,12 @@
 # coding=utf8
 
-from sae.const import (MYSQL_HOST, MYSQL_HOST_S,
-    MYSQL_PORT, MYSQL_USER, MYSQL_PASS, MYSQL_DB
-)
+# from sae.const import (MYSQL_HOST, MYSQL_HOST_S,
+#     MYSQL_PORT, MYSQL_USER, MYSQL_PASS, MYSQL_DB
+# )
 
 # SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1/test'
-SQLALCHEMY_DATABASE_URI = 'mysql://' + MYSQL_USER + ':' + MYSQL_PASS + '@' + MYSQL_HOST + ':' + MYSQL_PORT + '/' + MYSQL_DB
+SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@lnqvpyyubkyc.mysql.sae.sina.com.cn:10424/ticket'
+# SQLALCHEMY_DATABASE_URI = 'mysql://' + MYSQL_USER + ':' + MYSQL_PASS + '@' + MYSQL_HOST + ':' + MYSQL_PORT + '/' + MYSQL_DB
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 PAGE_SIZE = 50
