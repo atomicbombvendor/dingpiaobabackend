52083880c9a14345f805f290c27511994401b813
Unknown <atomicbombvendor@gmail.com>
2018-12-25 23:49:44 +0800

    1111

--- app/config.py
+++ app/config.py
@@ -1,8 +1,5 @@
 # coding=utf8
-
-
 SQLALCHEMY_DATABASE_URI = "mysql://j3y5w14ozm:zy523xiw1jjh1ljwji5zi2zw5ky0115mimimj405@w.rdc.sae.sina.com.cn:3306/app_xiaobaili"
-# SQLALCHEMY_DATABASE_URI = "mysql://root:123456@localhost:3306/test"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 PAGE_SIZE = 50
 TABLE_NAME = 'ticketinfo'
